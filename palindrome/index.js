8c9e81eadb46ac21e652f1a807aead29be66859c
morinoko <forby.1@gmail.com>
2020-02-27 22:03:29 +0900

    palindrome module

---
+++ palindrome/index.js
@@ -0,0 +1,28 @@
+module.exports = Phrase;
+
+// Add reverse function to the String protoype to add 'reverse()' to all strings
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
+}
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.letters().toLowerCase();
+  }
+
+  // Returns the letters in the content, without punctuation or spaces
+  this.letters = function letters() {
+    const letterRegex = /[a-z]/gi;
+
+    return (this.content.match(letterRegex) || []).join("");
+  }
+
+  // Returns true for a palindrome, false otherwise
+  this.palindrome = function palindrome() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
+}
